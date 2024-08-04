c1b746d96162cfca2f428b7f1b235a68930ee1ac
Apurbo <tanvirmahfuz22@gmail.com>
2024-08-04 23:46:57 +0600

    included chaye aur code

--- stack_heap.js
+++ stack_heap.js
@@ -1,14 +1,14 @@
-let x = "str"
-let y = x
-x = "str2"
-console.log(x)
-console.log(y)
+let x = "str";
+let y = x;
+x = "str2";
+console.log(x);
+console.log(y);
 
 let obj1 = {
-    name: "Tanvir",
-    age: 24
-}
-let obj2 = obj1
-obj1.name = "Mahfuz"
-console.log(obj1)
-console.log(obj2)
+  name: "Tanvir",
+  age: 24,
+};
+let obj2 = obj1;
+obj1.name = "Mahfuz";
+console.log(obj1);
+console.log(obj2);
